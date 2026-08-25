c82c15e965bfb2f571364cd4e7dd340fe7645258
mingchun <qqq1214q@email.com>
2026-08-25 15:23:04 +0800

    Rename public folder to docs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 4000;
 
 // Serve everything inside the public folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "docs")));
 
 app.listen(PORT, () => {
     console.log(`Website running at http://localhost:${PORT}`);
